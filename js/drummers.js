d06ba4fb9bf41bdf2e8cd22999824ed0cb856fe7
Kyle Taber <ktaber12@yahoo.com>
2017-04-18 15:24:56 -0400

    make links and opinion piece on musicians

--- js/drummers.js
+++ js/drummers.js
@@ -6,7 +6,7 @@ var drummer1 = {
     "id": 1,
     "name": "Glenn Kotche",
     "picture_url": "img/Drummers/glennkotcheportrait2.jpg",
-    "about": "Glenn Kotche born December 31 1970 in Roselle, Illinois, United States is an American drummer and composer, best known for his involvement in the band Wilco. He was named the 40th greatest drummer of all time by Gigwise in 2008. Prior to working with Wilco, Kotche released a four-track album entitled Introducing. In 2003, he released a second solo album titled Next, featuring solo drum improvisations using homemade percussion installations. A third solo album was released in March 2006 on Nonesuch Records, entitled Mobile. This album featured a broad range of compositions for percussion.",
+    "about": "Since 2001, Kotche has been the rhythmic anchor in Wilco, one of the most beloved rock bands on the planet. His first studio outing with the Chicago- based band was the breakthrough Yankee Hotel Foxtrot, and has continued over the course of five albums, including the Grammy-winning A Ghost Is Born and the critically acclaimed The Whole Love. He has appeared on over 80 recordings by artists as diverse as Andrew Bird, Edith Frost, Neil Finn and Radiohead's Phil Selway, and he’s a founding member of two other bands— Loose Fur, with Wilco frontman Jeff Tweedy and longtime collaborator Jim O’Rourke, and On Fillmore, with upright bassist Darin Gray. He has also written music for classical and post-classical ensembles like Kronos Quartet, the Silk Road Ensemble, the Bang on a Can All-Stars, So Percussion, eighth blackbird and many more.",
     "selling_points": [
         "Glenn Kotche Plays 'Art of Almost'",
         "'Wild Sound' An Arrangement by Glenn Kotche",
@@ -29,18 +29,19 @@ var drummer2 = {
     "id": 2,
     "name": "Bryan Devendorf",
     "picture_url": "img/Drummers/bryandevendorfportrait.jpg",
-    "about": "Glenn Kotche born December 31 1970 in Roselle, Illinois, United States is an American drummer and composer, best known for his involvement in the band Wilco. He was named the 40th greatest drummer of all time by Gigwise in 2008. Prior to working with Wilco, Kotche released a four-track album entitled Introducing. In 2003, he released a second solo album titled Next, featuring solo drum improvisations using homemade percussion installations. A third solo album was released in March 2006 on Nonesuch Records, entitled Mobile. This album featured a broad range of compositions for percussion.",
+    "about":"Don’t let him fool you—Bryan Devendorf is a rock star. As the drummer for the National, he exudes a quiet command of his craft, both on stage and in studio, creating powerful and creative beats that truly anchor his band’s trademark brand of dark, swelling indie rock. Just about every review of any National album will mention Devendorf ‘s unique, off-kilter drumming. But more drummers should take note of his creativity. He’s quite versatile, sometimes playing  jittery and driving rhythms on songs like “Graceless,” and taking a more minimal approach to percussion on quieter tracks like “Slipped” (both songs off their latest album Trouble Will Find Me). He’s more recognizable for the former approach to percussion, playing some of the coolest linear parts in music right now. Devendorf’s drums can take songs that don’t seem to have much rhythm or shape whatsoever and give them their punch, but his playing never feels invasive. A perfect example is “Squalor Victoria” off of their 2007 breakout album Boxer. His dancey, tribal march gives a song built around a droning violin and a lock-stepped piano groove its life and its sense of building dread. And live, he turns it into another beast, embellishing the intro with little variations without missing a step and expertly building to its chaotic finale. It’s really something to hear.",
     "selling_points": [
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        "The National play 'Squalor Victoria'",
+        "'Bloodbuzz Ohio' in the studio",
+        "'Mr. November' at the Brooklyn Academy of Music",
+        "An Interview with Bryan Devendorf"
+
     ],
     "linkURL": [
-      "https://www.youtube.com/watch?v=IkLCg6zNlz8",
-      "https://www.youtube.com/watch?v=D0S2aVUUGNs",
-      "https://www.youtube.com/watch?v=VwekSyWxcgg",
-      "https://youtu.be/OTdkHV9YsIk?t=1h44m20s",
-      "https://www.youtube.com/watch?v=y3paspn2oYw"
+      "https://www.youtube.com/watch?v=mZDZ1Wng9r0",
+      "https://www.youtube.com/watch?v=uKqI0FWb1Rw",
+      "https://www.youtube.com/watch?v=wLzChxmkH6s",
+      "https://www.youtube.com/watch?v=wAq_rL0JTmc"
     ]
 };
 
@@ -48,18 +49,19 @@ var drummer3 = {
     "id": 3,
     "name": "Justin Peroff",
     "picture_url": "img/Drummers/justinperoffportrait.jpg",
-    "about": "Glenn Kotche born December 31 1970 in Roselle, Illinois, United States is an American drummer and composer, best known for his involvement in the band Wilco. He was named the 40th greatest drummer of all time by Gigwise in 2008. Prior to working with Wilco, Kotche released a four-track album entitled Introducing. In 2003, he released a second solo album titled Next, featuring solo drum improvisations using homemade percussion installations. A third solo album was released in March 2006 on Nonesuch Records, entitled Mobile. This album featured a broad range of compositions for percussion.",
+    "about": "Justin Peroff has crafted some of the catchiest drum parts of the new millenium with Broken Social Scene.  Survey the past few years of indie music, and then go back and listen to You Forgot It In People. See what I mean? He transitions fluidly from offbeat rythms in titles such as '7/4 Shoreline' to abstract electronic tracks like 'All to All'. He has an ear for rhythm unlike most drummers of his time and boundless creativity that fits every style he comes across.",
     "selling_points": [
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        "Justin Peroff and Brendan Canning Make Video Game Music",
+        "Broken Social Scene plays '7/4 Shoreline'",
+        "An Interview with Stuart Berman and Justin Peroff",
+        "'All to All' an Electronic Drum Performance"
     ],
     "linkURL": [
-      "https://www.youtube.com/watch?v=IkLCg6zNlz8",
-      "https://www.youtube.com/watch?v=D0S2aVUUGNs",
-      "https://www.youtube.com/watch?v=VwekSyWxcgg",
-      "https://youtu.be/OTdkHV9YsIk?t=1h44m20s",
-      "https://www.youtube.com/watch?v=y3paspn2oYw"
+      "https://www.youtube.com/watch?v=Erl8PUUn8qA",
+      "https://www.youtube.com/watch?v=b8H5GlZqR4s",
+      "https://www.youtube.com/watch?v=k7oKAuWNJhc",
+      "https://www.youtube.com/watch?v=-9MAcFx7KFU"
+
     ]
 };
 
@@ -67,18 +69,23 @@ var drummer4 = {
     "id": 4,
     "name": "Philip Selway",
     "picture_url": "img/Drummers/philipselwayportrait.jpg",
-    "about": "Glenn Kotche born December 31 1970 in Roselle, Illinois, United States is an American drummer and composer, best known for his involvement in the band Wilco. He was named the 40th greatest drummer of all time by Gigwise in 2008. Prior to working with Wilco, Kotche released a four-track album entitled Introducing. In 2003, he released a second solo album titled Next, featuring solo drum improvisations using homemade percussion installations. A third solo album was released in March 2006 on Nonesuch Records, entitled Mobile. This album featured a broad range of compositions for percussion.",
+    "about": "Selway’s drumming stuck to the classic/alternative rock style for Radiohead’s first two album, Pablo Honey and The Bends. Once OK Computer rolled around, his skills became far more prominent as the music got more experimental. Selway’s ultimate drum track has to be “Pyramid Song”, from 2001’s Amnesiac. The mostly-piano driven piece really begins to take off about halfway through when Selway’s jazz influenced compound rhythm kicks in. For most people, this song would seem difficult to play along to due to Yorke’s frequent short pauses in the piano melody. However, Selway’s marching jazz beat provides the perfect support for Yorke’s piano and vocals to float over. Just because this was some of his best work, it doesn’t mean that he hasn’t had great tracks as of late. Listen to “There There”, “Weird Fishes/Arpeggi”, and “Reckoner”. You’ll hear the drum pattern of a man who evolved from playing straightforward rock beats to creating some of the most innovative rhythms of the past 20 years.",
     "selling_points": [
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "Lorem ip`sum dolor sit amet, consectetur adipisicing elit.",
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        "Radiohead performs 'Reckoner'",
+        "The Beautiful Tom-Tom work of 'Optimistic'",
+        "'15 Step' A Song Played in 5/4",
+        "A Highly Syncopated 'Myxomatosis'",
+        "Radiohead's 'Pyramind Song'",
+        "'Coming Up For Air' From Philip Selway's Solo Record"
+
     ],
     "linkURL": [
-      "https://www.youtube.com/watch?v=IkLCg6zNlz8",
+      "https://www.youtube.com/watch?v=8OS1U8LjjZw",
       "https://www.youtube.com/watch?v=D0S2aVUUGNs",
-      "https://www.youtube.com/watch?v=VwekSyWxcgg",
-      "https://youtu.be/OTdkHV9YsIk?t=1h44m20s",
-      "https://www.youtube.com/watch?v=y3paspn2oYw"
+      "https://www.youtube.com/watch?v=JrVKSKQKO1c",
+      "https://www.youtube.com/watch?v=fpk835d1K7U",
+      "https://www.youtube.com/watch?v=-ywtaz46F0I",
+      "https://www.youtube.com/watch?v=b2gMHd0Fpd0"
     ]
 };
 
@@ -86,18 +93,19 @@ var drummer5 = {
     "id": 5,
     "name": "Dan Snaith",
     "picture_url": "img/Drummers/dansnaithportrait.jpg",
-    "about": "Glenn Kotche born December 31 1970 in Roselle, Illinois, United States is an American drummer and composer, best known for his involvement in the band Wilco. He was named the 40th greatest drummer of all time by Gigwise in 2008. Prior to working with Wilco, Kotche released a four-track album entitled Introducing. In 2003, he released a second solo album titled Next, featuring solo drum improvisations using homemade percussion installations. A third solo album was released in March 2006 on Nonesuch Records, entitled Mobile. This album featured a broad range of compositions for percussion.",
+    "about": "The ease of modern music software has allowed for the creation of an abundance of average, homogenous, electronic records; but over the last 14 years, Dan Snaith's project, Caribou, has consistently stayed above the fray. There’s a point on “Mars,” the eighth and most sprawling track on the new Caribou album Our Love that offers a sonic snapshot of where Dan Snaith is now, six albums and one side project after his first EP release as Caribou (formerly Manitoba) in 2000. The track is all polyrhythmic drums and a sparse, staccato flute sample — minor key, more than a little foreboding, building to something, though it’s not quite clear what. Our Love is both a continuation of everything Snaith started with Jiaolong and a return to the florid, deeply emotional palette he’s been developing with Caribou, perfecting on 2010’s unimpeachable Swim.",
     "selling_points": [
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        "Lorem ip`sum dolor sit amet, consectetur adipisicing elit.",
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        "Caribou perform 'Our Love' on KCRW",
+        "An Oldschool Caribou Plays 'Yeti'",
+        "On Performing Live With Ableton",
+        "A Favorite Live Performance at The Pitchfork Music Festival"
     ],
     "linkURL": [
-      "https://www.youtube.com/watch?v=IkLCg6zNlz8",
-      "https://www.youtube.com/watch?v=D0S2aVUUGNs",
-      "https://www.youtube.com/watch?v=VwekSyWxcgg",
-      "https://youtu.be/OTdkHV9YsIk?t=1h44m20s",
-      "https://www.youtube.com/watch?v=y3paspn2oYw"
+      "https://www.youtube.com/watch?v=z3Zks4mDn5g",
+      "https://www.youtube.com/watch?v=2M8ZM0-pOSY",
+      "https://www.youtube.com/watch?v=8s7Z3vMUGrs",
+      "https://www.youtube.com/watch?v=j755GoKSrUE&t=1242s"
+
     ]
 
 };
@@ -114,9 +122,9 @@ var generateItem = function generateItem( musician ){
 
     $( "#drummer" + musician.id + " h2" ).text( musician.name );
     $( "#drummer" + musician.id + " img" ).attr( "src", musician.picture_url );
-    $( "#drummer" + musician.id + " ul" ).append(sellingPointsListItems);
+    $( "#drummer" + musician.id + " ul" ).html(sellingPointsListItems);
     $( "#drummer" + musician.id + " p" ).text( musician.about );
-    $( "#drummer" + musician.id + " span" ).text( musician.id );
+
 };
 
 var i = 0
